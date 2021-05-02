08ca25a325d18593af3801524bde0bdc959fada6
Sidharth <sidnagar08@gmail.com>
2021-05-02 23:47:41 +0530

    States

---
+++ src/components/Button.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+// function greet(){
+//     console.log("Hello!");
+// } 
+
+
+class Button extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            counter: 0
+        };
+        // this.handleButtonClick = this.handleButtonClick.bind('this');
+    }
+
+    handleButtonClick = () => {
+        this.setState({counter: 1});
+    }
+
+    render(){
+        return <button onClick={this.handleButtonClick}>{this.state.counter}</button>;
+    }
+}
+
+export default Button;
