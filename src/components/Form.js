af6064e2ec0bb7fa6adc62f9ae1ffa6d279faaec
Sidharth <sidnagar08@gmail.com>
2021-05-03 18:02:39 +0530

    Controlled Components

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,16 +2,26 @@ import React from "react"
 
 class Form extends React.Component{
 
+    constructor(){
+        super();
+        this.state = {
+            value: "Enter your name"
+        }
+    }
+
+    handleInputChange = (event) => {
+        this.setState({value: event.target.value});
+    }
+
     handleFormSubmit = (event) => {
         event.preventDefault();
-        let F_value = document.querySelector('#name').value;
-        alert(F_value);
+        alert(this.state.value);
     }
 
     render(){
         return(
             <form onSubmit={this.handleFormSubmit}>
-                <input type="text" id="name" />
+                <input type="text" id="name" value={this.state.value} onChange={this.handleInputChange} />
                 <input type="submit" />
             </form>
         );
