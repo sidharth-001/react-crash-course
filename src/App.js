87c700cdcf0d204b4dd40267c716b7464c6e2fb8
Sidharth <sidnagar08@gmail.com>
2021-05-04 21:42:57 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
+import axios from "axios";
 
 // import Form from "./components/Form";
 import Counter from "./components/Counter";
+import Test from "./components/Test";
 
 class App extends React.Component{
     render(){
         return (
             <div>
+                <Test />
                 <Counter />
             </div>
         )
